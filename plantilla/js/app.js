8ee9969d90133c33c3cff95a2a640c93d0fde3c7
Jonathan Padilla <jonathanpadilla09@outlook.com>
2016-12-22 18:47:09 -0400

    falta maquetear footer

---
+++ plantilla/js/app.js
@@ -0,0 +1,45 @@
+$(function(){
+
+	/*animacion menu*/
+	$("#bg-menu").sticky({ topSpacing: 0 });
+	$('#bg-menu').on('sticky-start', function() {
+	 	// $("#social-header").css('height', '0px');
+	 	$("#header").css('padding', '18px 0');
+
+	 	$("#social-header").css('height', '0');
+	 	$("#social-header ul").css('height', '0');
+
+	 	$("#logo img").css('width', '120px');
+	 	$("#logo img").css('margin-top', '0');
+
+	 	$("#space-header").css('height', '200px');
+	});
+
+	$('#bg-menu').on('sticky-end', function() {
+	 	// $("#social-header").css('height', '0px');
+	 	$("#header").css('padding', '0');
+
+	 	$("#social-header").css('height', '150px');
+	 	$("#social-header ul").css('height', 'inherit');
+
+	 	$("#logo img").css('width', '100%');
+	 	$("#logo img").css('margin-top', '30px');
+
+	 	$("#space-header").css('height', '210px');
+	});
+
+	/*hash menu*/
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	});
+
+});
