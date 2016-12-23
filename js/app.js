72357d297130ba4fba355c97f660d0fba9ad7e79
Jonathan Padilla <jonathanpadilla09@outlook.com>
2016-12-23 18:46:25 -0400

    responsiva

--- js/app.js
+++ js/app.js
@@ -4,13 +4,19 @@ $(function(){
 	$("#bg-menu").sticky({ topSpacing: 0 });
 	$('#bg-menu').on('sticky-start', function() {
 	 	// $("#social-header").css('height', '0px');
-	 	$("#header").css('padding', '18px 0');
 
 	 	$("#social-header").css('height', '0');
 	 	$("#social-header ul").css('height', '0');
 
-	 	$("#logo img").css('width', '120px');
-	 	$("#logo img").css('margin-top', '0');
+	 	if ((screen.width>=1024)) {
+	 		$("#header").css('padding', '18px 0');
+			$("#logo img").css('width', '120px');
+	 		$("#logo img").css('margin-top', '0');
+		}else{
+	 		$("#header").css('padding', '0');
+			$("#logo img").css('width', '0');
+			$("#menu-principal").css('margin-top', '20px');
+		}
 
 	 	$("#space-header").css('height', '200px');
 
@@ -19,13 +25,18 @@ $(function(){
 
 	$('#bg-menu').on('sticky-end', function() {
 	 	// $("#social-header").css('height', '0px');
-	 	$("#header").css('padding', '0');
 
 	 	$("#social-header").css('height', '150px');
 	 	$("#social-header ul").css('height', 'inherit');
 
-	 	$("#logo img").css('width', '100%');
-	 	$("#logo img").css('margin-top', '30px');
+	 	if ((screen.width>=1024)) {
+	 		$("#header").css('padding', '0');
+			$("#logo img").css('width', '100%');
+		 	$("#logo img").css('margin-top', '30px');
+		}else{
+			$("#logo img").css('width', '190px');
+			$("#menu-principal").css('margin-top', '150px');
+		}
 
 	 	$("#space-header").css('height', '210px');
 
@@ -46,15 +57,21 @@ $(function(){
 	    }
 	});
 
-	$(".item-producto").hover(function(){
+	$(".item-producto").hover(function(e){
 		var btn = $(this);
 		var id = btn.data('id');
+
+		e.preventDefault();
 
 		$(".fotos-producto").removeClass('mostrar');
 		$(".fotos-producto-"+id).addClass('mostrar');
 
 		$(".lista-productos-2 li a").removeClass('producto-seleccionado');
 		btn.addClass('producto-seleccionado');
+	});
+
+	$(".item-producto").on('click', function(e){
+		e.preventDefault();
 	});
 
 	var map = new GMaps({
