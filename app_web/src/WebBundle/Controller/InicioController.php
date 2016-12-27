6141052c5fa7e4a210924b3636753145546812ba
Jonathan Padilla <jonathanpadilla09@outlook.com>
2016-12-27 13:01:29 -0400

    menu responsive

--- app_web/src/WebBundle/Controller/InicioController.php
+++ app_web/src/WebBundle/Controller/InicioController.php
@@ -138,6 +138,45 @@ class InicioController extends Controller
         exit;
     }
 
+    public function enviarContactoAction(Request $request)
+    {
+        $result = true;
+
+        $datos_mail = array(
+            'nombre'        => ucwords($request->get('nombre')),
+            'correo'        => $request->get('correo'),
+            'telefono'      => $request->get('telefono'),
+            'mensaje'       => $request->get('mensaje'),
+            'from'          => 'jonathanpadilla09@outlook.com',
+            'to'            => 'jonathanpadilla0109@gmail.com',
+        );
+
+        $this->enviarMail($datos_mail);
+
+        echo json_encode(array('result' => $result));
+        exit;
+    }
+
+    private function enviarMail($arr = false)
+    {
+        // print_r($productos);
+        $return = false;
+        if(is_array($arr))
+        {
+            $headers = "From: ".$arr['from']."\r\n";
+            $headers .= "Reply-To: ".$arr['to']."\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+            $contenido = $this->renderView('WebBundle::plantilla_contacto.html.twig', array('datos' => $arr));
+            // echo $contenido; exit;
+            if(mail($arr['to'], 'www.greenwalk.cl - '.$arr['nombre'], $contenido, $headers))
+            {
+                $return = $contenido;
+            }
+        }
+        return $return;
+    }
+
     private function subirImagen($foto)
     {
         $result = null;
--- js/app.js
+++ js/app.js
@@ -242,4 +242,81 @@ $(function(){
 
 	});
 
+	$("#btn_enviar").on('click', function(e){
+
+		var form_contacto = $("#form_contacto");
+
+		form_contacto.validate({
+            rules: {
+
+                nombre: {
+					required: true
+				},
+				correo: {
+					required: true,
+					email: true
+				},
+				telefono: {
+					required: true,
+					number: true,
+				},
+                mensaje: {
+                    required: true
+                }
+            },
+            messages: {
+
+                nombre: {
+					required: 'Campo requerido'
+				},
+				correo: {
+					required: 'Campo requerido',
+					email: 'Ingrese un correo válido'
+				},
+				telefono: {
+					required: 'Campo requerido',
+					number: 'Ingrese un numero válido',
+				},
+                mensaje: {
+                    required: 'Campo requerido'
+                }
+            },
+            errorPlacement: function(error, element) {
+                if (element.is(":radio") || element.is(":checkbox")) {
+                    element.closest('.option-group').after(error);
+                }
+                else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+
+		e.preventDefault();
+
+		if(form_contacto.valid(true))
+		{
+	        $.ajax({
+	            url: Routing.generate('ajax_enviar_contacto'),
+	            data: $('#form_contacto').serialize(),
+	            dataType: 'json',
+	            method: 'post'
+	        }).done(function(json){
+	            if(json.result)
+	            {
+	            	location.reload(true);
+	            }
+	        });
+
+		}
+
+        
+    });
+
+document.querySelector('.boton-menu').onclick = mostrarMenu;
+
 });
+
+function mostrarMenu() {
+    var nav = document.querySelector('.menu');
+    nav.classList.toggle('mostrar');
+}
